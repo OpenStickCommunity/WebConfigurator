ecbe14a245f17f559d17130d60f803ec33d75601
deeebug <77402236+deeebug@users.noreply.github.com>
2023-02-08 22:54:38 +0545

    Add reboot button to web configurator (#16)

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,14 +1,27 @@
-import React, { useContext } from 'react';
-import { Nav, NavDropdown, Navbar } from 'react-bootstrap';
+import React, { useContext, useState } from 'react';
+import { Nav, NavDropdown, Navbar, Button, Modal } from 'react-bootstrap';
 import { NavLink } from "react-router-dom";
 import { AppContext } from '../Contexts/AppContext';
 import FormSelect from './FormSelect';
 import { saveButtonLabels } from '../Services/Storage';
 import BUTTONS from '../Data/Buttons.json';
 import './Navigation.scss';
+import WebApi from '../Services/WebApi';
 
 const Navigation = (props) => {
 	const { buttonLabels, setButtonLabels } = useContext(AppContext);
+
+	const [show, setShow] = useState(false);
+	const [isRebooting, setIsRebooting] = useState(null); // null because we want the button to assume untouched state
+
+	const handleClose = () => setShow(false);
+	const handleShow = () => { setIsRebooting(null); setShow(true); }
+	const handleReboot = async () => {
+		if (isRebooting == false) { setShow(false); return; }
+		setIsRebooting(true);
+		await WebApi.reboot();
+		setIsRebooting(false);
+	};
 
 	const updateButtonLabels = (e) => {
 		saveButtonLabels(e.target.value);
@@ -39,20 +52,41 @@ const Navigation = (props) => {
 						<NavDropdown.Item as={NavLink} exact={true} to="/reset-settings">Reset Settings</NavDropdown.Item>
 					</NavDropdown>
 				</Nav>
+				<Nav>
+					<Button style={{ marginRight: "7px" }} variant="success" onClick={handleShow}>
+						Reboot
+					</Button>
+					<div style={{ marginTop: "4px" }}>
+						<FormSelect
+							name="buttonLabels"
+							className="form-select-sm"
+							value={buttonLabels}
+							onChange={updateButtonLabels}
+						>
+							{Object.keys(BUTTONS).map((b, i) =>
+								<option key={`button-label-option-${i}`} value={BUTTONS[b].value}>{BUTTONS[b].label}</option>
+							)}
+						</FormSelect>
+						<Navbar.Toggle aria-controls="basic-navbar-nav" />
+					</div>
+				</Nav>
 			</Navbar.Collapse>
-			<div className="navbar-tools">
-				<FormSelect
-					name="buttonLabels"
-					className="form-select-sm"
-					value={buttonLabels}
-					onChange={updateButtonLabels}
-				>
-					{Object.keys(BUTTONS).map((b, i) =>
-						<option key={`button-label-option-${i}`} value={BUTTONS[b].value}>{BUTTONS[b].label}</option>
-					)}
-				</FormSelect>
-				<Navbar.Toggle aria-controls="basic-navbar-nav" />
-			</div>
+
+			<Modal show={show} onHide={handleClose}>
+				<Modal.Header closeButton>
+					<Modal.Title>Reboot controller?</Modal.Title>
+				</Modal.Header>
+				<Modal.Body>{ isRebooting == false ? "Done rebooting, this browser tab can now be closed."
+								: "Reboot to regular controller mode to play?" }</Modal.Body>
+				<Modal.Footer>
+					<Button variant="secondary" onClick={handleClose}>
+						No
+					</Button>
+					<Button variant="primary" onClick={handleReboot}>
+						{ isRebooting == null ? "Yes" : (isRebooting ? "Rebooting" : "Done!") }
+					</Button>
+				</Modal.Footer>
+			</Modal>
 		</Navbar>
 	);
 };
